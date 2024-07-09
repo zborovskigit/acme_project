a4f4f6c6ccc459633de61bca0f2cf488688d6c36
Ivan <me@zborovski.ru>
2024-07-09 17:00:00 +0300

    Refactored edit, delete views in CBV

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.BirthdayCreateView.as_view(), name='create'),
     path('list/', views.BirthdayListView.as_view(), name='list'),
     path('<int:pk>/edit/', views.BirthdayUpdateView.as_view(), name='edit'),
-    path('<int:pk>/delete/', views.delete_birthday, name='delete'),
+    path('<int:pk>/delete/', views.BirthdayDeleteView.as_view(), name='delete'),
 ]
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,24 +1,41 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator
-from django.views.generic import CreateView, ListView, UpdateView
+# from django.shortcuts import render, get_object_or_404, redirect
+# from django.core.paginator import Paginator
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
-from .utils import calculate_birthday_countdown
 from .models import Birthday
+# from .utils import calculate_birthday_countdown
+
 
 
 class BirthdayMixin:
     model = Birthday
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
     success_url = reverse_lazy('birthday:list')
 
-class BirthdayCreateView(BirthdayMixin, CreateView):
+
+class BirthdayFormMixin:
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+
+
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
     pass
 
-class BirthdayUpdateView(BirthdayMixin, UpdateView):
+
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
+    pass
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
     pass 
+
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 2
+
 
 # def birthday(request, pk=None):
 #     if pk is not None:
@@ -37,19 +54,14 @@ class BirthdayUpdateView(BirthdayMixin, UpdateView):
 #         context.update({'birthday_countdown': birthday_countdown})
 #     return render(request, 'birthday/birthday.html', context)
 
-def delete_birthday(request, pk):
-    instance = get_object_or_404(Birthday, pk=pk)
-    form = BirthdayForm(instance=instance)
-    context = {'form': form}
-    if request.method == 'POST':
-        instance.delete()
-        return redirect('birthday:list')
-    return render(request, 'birthday/birthday.html', context)
-
-class BirthdayListView(ListView):
-    model = Birthday
-    ordering = 'id'
-    paginate_by = 2
+# def delete_birthday(request, pk):
+#     instance = get_object_or_404(Birthday, pk=pk)
+#     form = BirthdayForm(instance=instance)
+#     context = {'form': form}
+#     if request.method == 'POST':
+#         instance.delete()
+#         return redirect('birthday:list')
+#     return render(request, 'birthday/birthday.html', context)
 
 # def birthday_list(request):
 #     template = 'birthday/birthday_list.html'
