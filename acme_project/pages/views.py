7087225ab5f3153aa4601f9ee9a0a8d024cf6e67
zborovski <me@zborovski.ru>
2024-07-10 15:47:49 +0300

    Added birthdays total count

--- acme_project/pages/views.py
+++ acme_project/pages/views.py
@@ -1,5 +1,18 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from birthday.models import Birthday
 
 
 def homepage(request):
     return render(request, 'pages/index.html')
+
+
+class HomePage(TemplateView):
+    template_name = 'pages/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_count'] = Birthday.objects.count()
+        return context
