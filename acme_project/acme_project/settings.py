21e4639920db0953fcfbce5db4787b37a1043ce4
zborovski <me@zborovski.ru>
2024-07-22 16:26:51 +0300

    Added .filebased send_mail() for admin

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -89,3 +89,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+# EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
 
 from .models import Birthday
 
@@ -26,6 +27,13 @@ class BirthdayForm(forms.ModelForm):
         first_name = self.cleaned_data['first_name']
         last_name = self.cleaned_data['last_name']
         if f'{first_name} {last_name}' in BEATLES:
+            send_mail(
+                subject='Another Beatles member',
+                message=f'{first_name} {last_name} пытался опубликовать запись!',
+                from_email='birthday_form@acme.not',
+                recipient_list=['admin@acme.not'],
+                fail_silently=True,
+            )
             raise ValidationError(
                 'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
             )
