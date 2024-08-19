eaaa9b5ee575bb5cf9106057dbb20cbfc586a1cd
zborovski <me@zborovski.ru>
2024-08-19 11:41:59 +0300

    Added buttons in header and registration page

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,7 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
+
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
 
 urlpatterns = [
@@ -9,4 +12,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
