9ee72a2c908936bc7ef8dc171637eb4a7fac072b
zborovski <me@zborovski.ru>
2024-09-19 11:13:09 +0300

    Added Congratulation model

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -34,3 +34,17 @@ class Birthday(models.Model):
 
     def get_absolute_url(self):
             return reverse('birthday:detail', kwargs={"pk": self.pk})
+
+
+class Congratulation(models.Model):
+    text = models.TextField('Текст поздравления')
+    birthday = models.ForeignKey(
+        Birthday,
+        on_delete=models.CASCADE,
+        related_name='congratulations',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('created_at',)
